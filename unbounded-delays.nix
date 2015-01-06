76d08ba5738acd6309e0d41a0a2fa8056971688f
Bas van Dijk <v.dijk.bas@gmail.com>
2015-01-06 09:58:56 +0000

    Added shell.nix

--- unbounded-delays.nix
+++ unbounded-delays.nix
@@ -2,7 +2,7 @@
 
 cabal.mkDerivation (self: {
   pname = "unbounded-delays";
-  version = "0.1.0.8";
+  version = "HEAD";
   src = ./.;
   meta = {
     homepage = "https://github.com/basvandijk/unbounded-delays";
