2b81a00c5882f6e0577033144f0ae3fe15ea02de
Bas van Dijk <v.dijk.bas@gmail.com>
2017-03-27 20:08:00 +0200

    Update Nix files

--- default.nix
+++ default.nix
@@ -1,2 +1,15 @@
-let pkgs = import <nixpkgs> {};
-in pkgs.haskellPackages.callPackage ./unbounded-delays.nix {}
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage (import ./unbounded-delays.nix) {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
--- unbounded-delays.nix
+++ unbounded-delays.nix
@@ -1,13 +1,10 @@
-{ cabal }:
-
-cabal.mkDerivation (self: {
+{ mkDerivation, base, stdenv }:
+mkDerivation {
   pname = "unbounded-delays";
   version = "HEAD";
   src = ./.;
-  meta = {
-    homepage = "https://github.com/basvandijk/unbounded-delays";
-    description = "Unbounded thread delays and timeouts";
-    license = self.stdenv.lib.licenses.bsd3;
-    platforms = self.ghc.meta.platforms;
-  };
-})
+  libraryHaskellDepends = [ base ];
+  homepage = "https://github.com/basvandijk/unbounded-delays";
+  description = "Unbounded thread delays and timeouts";
+  license = stdenv.lib.licenses.bsd3;
+}
